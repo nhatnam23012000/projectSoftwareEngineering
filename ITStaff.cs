fcd28a8e4e492f3ba0167f0c307aecd1c76733c6
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2020-06-15 20:26:23 +0700

    Phuoc add ITStaff.cs

---
+++ ITStaff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFCS
+{
+    public class ITStaff : User
+    {
+        public ITStaff(Account account) : base(account) { }
+        public void createStaffAccount(String name, String pass, Authorization authorization, int id, AccountDatabase database)
+        {
+            database.addStaffAccount(name, pass, authorization, id);
+        }
+        public StallDatabase editStallDatabase(StallDatabase database)
+        {
+            return database;
+        }
+    }
+}

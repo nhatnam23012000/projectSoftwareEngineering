844b21dbf50ad30603cb698f6b503e9b018f4ad4
Nam Nguyen <nhatnam23012000@gmail.com>
2020-06-15 12:29:55 +0700

    add class cook

---
+++ Cook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFCS
+{
+    public class Cook : User
+    {
+        public Cook(Account account) : base(account) { }
+        public Order viewOrder(int id, OrderDatabase database)
+        {
+            return database.getOrder(id);
+        }
+        public void setOrderState(int id, State state, OrderDatabase database)
+        {
+            database.getOrder(id).setState(state);
+        }
+        public String notification(int id, OrderDatabase database)
+        {
+            if (database.getOrder(id).getState() == State.READY)
+            {
+               return "Your order is ready!";
+            }
+            return "Your order is still in process";
+        }
+    }    
+}

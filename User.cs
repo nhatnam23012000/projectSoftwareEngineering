83ab294fb7eab6890d77b8e0ad61a76614638bc3
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2020-06-15 20:40:11 +0700

    Phuoc add User.cs

---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFCS
+{
+    public abstract class User
+    {
+        protected Account account;
+        public User(Account account)
+        {
+            this.account = account;
+        }
+        public Account getAccount()
+        {
+            return account;
+        }
+    }
+}
